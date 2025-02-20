2e3684ec657fc0c1a59d455f79b147124204cbe6
Irsor <skar67@yandex.ru>
2025-02-20 20:43:53 +0300

    [+] Добавил GLFW и fmt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
-﻿#include <iostream>
+﻿#include <fmt/core.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit())
+    {
+        fmt::println("Failed to initialize GLFW");
+        return -1;
+    }
+
+    auto window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window)
+    {
+        fmt::println("Failed to create window");
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
     return 0;
 }
